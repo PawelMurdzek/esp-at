6a12ecd5c11adb2c871861f3877586226f17fac4
PawelMurdzek <105887950+PawelMurdzek@users.noreply.github.com>
2024-07-17 17:01:59 +0200

    changes to sendbytes and back to otiginal repo

--- main/interface/uart/at_uart_task.c
+++ main/interface/uart/at_uart_task.c
@@ -30,12 +30,47 @@ static QueueHandle_t s_at_uart_queue = NULL;
 uint8_t g_at_cmd_port = UART_NUM_1;
 at_uart_port_pins_t g_uart_port_pin;
 
+#define BUFFER_SIZE 1024
+
+static uint8_t buffer[BUFFER_SIZE];
+static uint32_t buffer_len = 0;
+
 static int32_t at_uart_write_data(uint8_t *data, int32_t len)
 {
     uint32_t length = 0;
-    uart_wait_tx_done(g_at_cmd_port,portMAX_DELAY);
-    length = uart_write_bytes(g_at_cmd_port, (char *)data, len);
-    uart_wait_tx_done(g_at_cmd_port,portMAX_DELAY);
+    if (len > 128)
+    {
+        // If there's data in the buffer, try to send it first
+        if (buffer_len > 0)
+        {
+            uart_wait_tx_done(g_at_cmd_port, portMAX_DELAY);
+            length = uart_write_bytes(g_at_cmd_port, (char *)buffer, buffer_len);
+            uart_wait_tx_done(g_at_cmd_port, portMAX_DELAY);
+            buffer_len = 0; // Clear the buffer after sending
+        }
+
+        // Check if the incoming data fits in the buffer
+        if (len > BUFFER_SIZE)
+        {
+            // If the data is too large to fit in the buffer, send it directly
+            uart_wait_tx_done(g_at_cmd_port, portMAX_DELAY);
+            length += uart_write_bytes(g_at_cmd_port, (char *)data, len);
+            uart_wait_tx_done(g_at_cmd_port, portMAX_DELAY);
+        }
+        else
+        {
+            // Otherwise, store the data in the buffer for later sending
+            memcpy(buffer, data, len);
+            buffer_len = len;
+        }
+    }
+    else
+    {
+        // If the length is less than or equal to 128, send it directly
+        uart_wait_tx_done(g_at_cmd_port, portMAX_DELAY);
+        length = uart_write_bytes(g_at_cmd_port, (char *)data, len);
+        uart_wait_tx_done(g_at_cmd_port, portMAX_DELAY);
+    }
     return length;
 }
 
