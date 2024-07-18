51f34e4c6ea7edc3f7179771846e83dec99ef369
PawelMurdzek <105887950+PawelMurdzek@users.noreply.github.com>
2024-07-18 17:25:09 +0200

    16k TX and RX

--- main/interface/include/at_uart.h
+++ main/interface/include/at_uart.h
@@ -42,6 +42,7 @@
 #define AT_UART_RX_BUFFER_SIZE                      2048                    /**< uart rx buffer size */
 // #define AT_UART_TX_BUFFER_SIZE                      8192*2                   /**< uart tx buffer size */
 // #define AT_UART_RX_BUFFER_SIZE                      256                    /**< uart rx buffer size */
+#define AT_UART_RX_BUFFER_SIZE                      16384                    /**< uart rx buffer size */
 #define AT_UART_TX_BUFFER_SIZE                      16384                   /**< uart tx buffer size */
 
 #define AT_UART_QUEUE_SIZE                          30                      /**< uart queue size */
